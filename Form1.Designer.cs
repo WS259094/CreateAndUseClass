880fcf41128237a0df0e924a4bca77d7d31b970c
WS259094 <WS259094@Weston.ac.uk>
2019-10-03 10:21:00 +0100

    GetSet
    Get and set classes done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(77, 45);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(77, 91);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(68, 167);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(13, 51);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(16, 97);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "Age";
             // 
             // CreateUseClass1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(219, 238);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
             this.Name = "CreateUseClass1";
             this.Text = "Create And Use Class";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,60 @@ namespace CreateAndUseClass
         {
             InitializeComponent();
         }
+
+        // clsUser starts here. it gets and sets the users name and age for later use in the program
+        class clsUser
+        {
+            private string m_sName;
+            private int m_iAge;
+
+            public string GetName(string sName)
+            {
+                return m_sName;
+            }
+            
+            public void SetName(string sName)
+            {
+                m_sName = sName;
+            }
+
+             public int GetAge(int iAge)
+            {
+                return m_iAge;
+            }
+            public void SetAge(int iAge)
+            {
+                m_iAge = iAge;
+            }
+        }
+
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            clsUser objUser = new clsUser();
+            if(txtName.Text == "" || txtAge.Text == "")
+            {
+                MessageBox.Show("Error. Please fill in all details");
+            }
+
+            else
+            {
+                objUser.SetName(txtName.Text);
+
+                /*Testing if the Age is a number using try parse, which checks if the text is in the correct format, e.g. int,
+                 * and parse, which converts the text to the correct format */
+                if(int.TryParse(txtAge.Text, out int iOut))
+                {
+                    objUser.SetAge(int.Parse(txtAge.Text));
+                }
+
+                else
+                {
+                    MessageBox.Show("Error. Age must be a number");
+                }
+            }
+            
+
+            Console.Beep(20, 20);
+        }
     }
 }
